c59bd19fd5ff83e16dd31a168342c83fbf94336f
mmoussel <mustafa@finflx.com>
2023-11-12 12:05:57 +0200

    feat: setup cookie session

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,23 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const cookieSession = require('cookie-session');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cookieSession({
+      keys: ['asdf'],
+    }),
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
   await app.listen(3000);
 }
 bootstrap();
