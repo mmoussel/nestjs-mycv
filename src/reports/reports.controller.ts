f0af59061b687a3f487a29493a8b6d2815f56b3f
mmoussel <mustafa@finflx.com>
2023-11-13 19:51:44 +0200

    feat: implement reports services

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { UseAuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { ReportDto } from './dtos/report.dto';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 
 @Controller('reports')
-export class ReportsController {}
+@UseAuthGuard()
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Post()
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
+  }
+
+  @Patch('/:id')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportsService.changeApproval(id, body.approved);
+  }
+
+  @Get()
+  getReports() {
+    return this.reportsService.find();
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './report.entity';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { User } from '../users/user.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report) private reportsRepo: Repository<Report>,
+  ) {}
+
+  create(reportDto: CreateReportDto, user: User) {
+    const report = this.reportsRepo.create(reportDto);
+
+    report.user = user;
+
+    return this.reportsRepo.save(report);
+  }
+
+  find() {
+    return this.reportsRepo.find();
+  }
+
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.reportsRepo.findOne({
+      where: { id: parseInt(id) },
+    });
+
+    if (!report) {
+      throw new Error('report not found');
+    }
+
+    report.approved = approved;
+
+    return this.reportsRepo.save(report);
+  }
+}
